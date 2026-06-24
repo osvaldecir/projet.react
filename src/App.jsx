a73d6f73ff00739acad830592c5c95c2bf609aff
osvaldecir viana <manocatanduva@gmail.com>
2026-06-24 08:36:07 -0300

    //

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+  const message = "Hello World";
+  return (
+    <div>
+      <h1>{message}</h1>
+      <h1>{message}</h1>
+      <h1>{message}</h1>
+      <h1>{message}</h1>
+      <h1>{message}</h1>
+      <h1>{message}</h1>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
