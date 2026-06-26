876f1f438b5e1f1054e04e3d9cc6ab64b4c8613f
osvaldecir viana <manocatanduva@gmail.com>
2026-06-26 04:42:27 -0300

    //

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
-import "./App.css";
+
 
 function App() {
   return (
     <div>
-      <h1 className="title">Gerenciador de Tarefas</h1>
+      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
       <AddTask />
       <Tasks />
     </div>
   );
 }
-
+ 
 
 export default App;
