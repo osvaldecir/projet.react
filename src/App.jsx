80d10b3f62c8072baf0d7551b05410d67b2020cf
osvaldecir viana <manocatanduva@gmail.com>
2026-06-28 06:02:23 -0300

    //

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import Tasks from "./components/Tasks";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500 text-3xl">Gerenciador de Tarefas</h1>
-      <AddTask />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-400 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks />
+      </div>
     </div>
   );
 }
